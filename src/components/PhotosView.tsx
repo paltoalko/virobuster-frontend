2794752204c809380cae41ccc83200fac34ee678
paltoalko <martas90909@me.com>
2022-11-01 21:26:13 +0000

    isolated textpoup for reusability

--- src/components/PhotosView.tsx
+++ src/components/PhotosView.tsx
@@ -1,45 +1,11 @@
 import { Box, Typography } from '@mui/material';
 import React from 'react';
+import TextPopUp from '../assets/helpers/TextPopUp';
 import styles from '../assets/styles/PhotosView.module.css';
 import { ReactComponent as Lab } from '../assets/svg/lab.svg';
 import { ReactComponent as Room } from '../assets/svg/room.svg';
 import { ReactComponent as Safe } from '../assets/svg/safe.svg';
-import { ReactJSXElement } from '@emotion/react/types/jsx-namespace';
 import CallToAction from './CallToAction';
-
-interface TextPopUpProps {
-  text1: string;
-  text2: string;
-  svg: ReactJSXElement;
-  left?: boolean;
-}
-
-const TextPopUp: React.FC<TextPopUpProps> = ({ text1, text2, svg, left }) => {
-  return (
-    <Box className={styles.content}>
-      {left && svg}
-      <Box className={styles.textContainer}>
-        <Typography
-          variant="body1"
-          color="primary"
-          className={styles.textElement}
-          fontWeight={200}
-        >
-          {text1}
-        </Typography>
-        <Typography
-          variant="body1"
-          color="primary"
-          className={styles.textElement}
-          fontWeight={200}
-        >
-          {text2}
-        </Typography>
-      </Box>
-      {!left && svg}
-    </Box>
-  );
-};
 
 const PhotosView: React.FC<{}> = ({}) => {
   return (
