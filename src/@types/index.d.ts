a837e1a7f7e251a6c7a93a810f94e784e7d4e05c
paltoalko <martas90909@me.com>
2022-11-29 16:09:14 +0000

    jpeg imports

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -9,6 +9,10 @@ declare module '*.jpg' {
   const content: string;
   export default content;
 }
+declare module '*.jpeg' {
+  const content: string;
+  export default content;
+}
 
 declare module '*.png' {
   const content: string;
